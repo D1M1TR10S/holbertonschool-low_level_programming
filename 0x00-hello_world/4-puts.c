6c325cf11e3f5d2164107ca06785c571d0a803ca
DimitiCat <dimitriosphilliou@gmail.com>
2017-09-14 05:53:48 +0000

    Added comment and int to 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-char main(void)
+/*prints Programming is like... & returns the value 0 */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
