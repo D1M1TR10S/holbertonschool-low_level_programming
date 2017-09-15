12186402bbfda30110afdbeff014e76211724736
DimitiCat <dimitriosphilliou@gmail.com>
2017-09-15 02:33:48 +0000

    Made 3 betty compliant

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,19 +4,18 @@
  *
  * Return: Always 0 (Success)
  */
-	int main(void)
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch = ch + 1)
 	{
-		    int ch;
-
-		    for (ch = 'a'; ch <= 'z'; ch = ch + 1)
-		    {
-			    putchar(ch);
-		    }
-		    for (ch = 'A'; ch <= 'Z'; ch = ch + 1)
-		    {
-			    putchar(ch);
-		    }
-		    putchar('\n');
-		    return (0);
+		putchar(ch);
 	}
-
+	for (ch = 'A'; ch <= 'Z'; ch = ch + 1)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+	}
