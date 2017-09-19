e9081d76bd9e1fb01bfe07cc9cef017552560e5c
DimitiCat <dimitriosphilliou@gmail.com>
2017-09-19 02:39:48 +0000

    Made alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_alphabet -prints the alphabet
+ * void: no argument
+ * Return:
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
