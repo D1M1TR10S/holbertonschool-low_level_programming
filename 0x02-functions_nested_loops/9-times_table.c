ece4d5bd60e425420631843ad9ec5a55dff562e3
DimitiCat <dimitriosphilliou@gmail.com>
2017-09-20 20:38:37 +0000

    Made the times tablegit add .! Like a boss!

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,34 @@
 #include "holberton.h"
 /**
- * 
+ * times_table - Make a times table
  *
- *
+ * Return: void
  */
+void times_table(void)
+{
+	int x, y, z;
+
+	for (y = 0; y <= 9; y++)
+	{
+		for (x = 0; x <= 9; x++)
+		{
+			z = x * y;
+
+			if ((z < 10) && (x != 0))
+			{
+				_putchar(' ');
+			}
+			else if (z > 9)
+			{
+				_putchar(z / 10 + 48);
+			}
+			_putchar(z % 10 + 48);
+			if (x != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
