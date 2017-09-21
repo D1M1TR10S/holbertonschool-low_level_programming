6feb43d17e3d22cfcc50955fd32a1fa209604815
DimitiCat <dimitriosphilliou@gmail.com>
2017-09-21 23:12:10 +0000

    Made 2-mul.c.

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * mul - Multiplies two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
