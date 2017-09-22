5674322a6c79342f4ea0835b1f0bb1e4d711078c
DimitiCat <dimitriosphilliou@gmail.com>
2017-09-22 20:00:01 +0000

    Made 101 Betty compliant.

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 /**
- * main - Print largest prime factor of a number
- * Return: Always 0
+ * print_number - Print numbers of main.c file
+ * @n: Integer of input
+ * Return: void
  */
 void print_number(int n)
 {
 	unsigned int l, x, d;
-	
+
 	l = n;
 	x = n;
 	d = 1;
