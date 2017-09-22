8500163ef792802e575cb1944c261737af4e6e16
DimitiCat <dimitriosphilliou@gmail.com>
2017-09-22 04:23:34 +0000

    Made FizzBuzzgit add .!

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Print Fizz for Multiples of 3
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 1;
+
+	while (n <= 100)
+	{
+		if (n % 3 == 0)
+			printf("Fizz");
+
+		else if (n % 5 == 0)
+			printf("Buzz");
+
+		else if ((n % 3 == 0) && (n % 5 == 0))
+			printf("FizzBuzz");
+
+		else
+			printf("%d", n);
+
+		printf(" ");
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
