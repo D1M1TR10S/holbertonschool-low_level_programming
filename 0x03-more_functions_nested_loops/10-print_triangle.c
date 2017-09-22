c1dce7f951328263bfbfa704adb0120bbb77824a
DimitiCat <dimitriosphilliou@gmail.com>
2017-09-22 05:47:24 +0000

    Made 10-print_triangle.c and it effin worked!

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * print_triangle - Print a triangle of # signs
+ * Return: 0
+ */
+void print_triangle(int size)
+{
+	int x, y, z;
+
+	if (size > 0)
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (z = size - x; z > 0; z--)
+			{
+				_putchar(' ');
+			}
+			for (y = 0; y < x; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
