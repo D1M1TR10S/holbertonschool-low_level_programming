63d8bf9df65c85983d4c0ff0496dde94752d7ca2
DimitiCat <dimitriosphilliou@gmail.com>
2017-09-22 02:37:17 +0000

    Made 7-print_diagonalgit add .!

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_diagonal - Prints a diagonal line across the screen.
+ * @n: number of spaces before diagonal line.
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int x, s;
+	
+	if (n < 1)
+		_putchar('\n');
+	else
+	{
+		for (x = 1; x <= n; x++)
+		{
+			for (s = 1; s < x; s++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
