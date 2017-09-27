2d267cc064c1dc07dff43939b669d4239e0a70e0
DimitiCat <dimitriosphilliou@gmail.com>
2017-09-27 02:56:31 +0000

    Made 3-puts.c

---
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _puts - Printing string into standard output.
+ * @str: Character string.
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int a = 0;
+	
+	while (str[a] != '\0')
+	{	
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
