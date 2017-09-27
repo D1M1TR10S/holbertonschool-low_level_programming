b4b61d6d5f8a11ab7a9cb651a0ef20c8b9e3b27f
DimitiCat <dimitriosphilliou@gmail.com>
2017-09-27 21:43:56 +0000

    Made 7-puts_half

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * puts_half - Cuts string in half and return.
+ * @str: Pointer referring to string
+ * Return: Nothing
+ */
+void puts_half(char *str)
+{
+	int n = 0;
+
+	while (str[n] != '\0')
+	{
+		n++;
+	}
+	n = n / 2;
+	while (str[n] != '\0')
+	{
+		_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');
+}
