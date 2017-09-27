d3196041f2f3868364501024564c083fbfa62a80
DimitiCat <dimitriosphilliou@gmail.com>
2017-09-27 02:02:04 +0000

    Made 2-strlen using square brackets in the while loop.

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _strlen - Print the length of the string
+ * @s: Prints length of the string "Holberton"
+ * Return: l
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+	return (l);
+}
