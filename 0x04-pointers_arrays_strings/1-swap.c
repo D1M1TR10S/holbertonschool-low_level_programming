3ae92352c842e4e2b7db1aa8951069443aedf55e
DimitiCat <dimitriosphilliou@gmail.com>
2017-09-27 01:05:51 +0000

    Made 1-swap.c with pointers on a and b!

---
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * swap_int - Swap values of integers a & b
+ * @a: Turns from 98 to 42
+ * @b: Turns from 42 to 98
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+	
+	c = *a;
+	*a = *b;
+	*b = c;
+}
