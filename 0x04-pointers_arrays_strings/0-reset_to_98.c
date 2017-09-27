37d2355e127fd048500c087869d02961b6ed4204
DimitiCat <dimitriosphilliou@gmail.com>
2017-09-27 00:21:03 +0000

    Made pointers & arrays directory and 0-reset_to_98 file.

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - Changes int pointed to to a 98 in main.c file
+ * @n: Integer with a pointer changing it to 98
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
