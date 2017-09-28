568507c21841a4bb4dc03505b2b7fb9eb2a1ede1
DimitiCat <dimitriosphilliou@gmail.com>
2017-09-28 22:11:47 +0000

    Made 1-strncat.cgit add .! Woot woot!

---
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * *_strcat - concatenates two strings together.
+ * @dest: src is appending string to the end of dest
+ * @src: string being appended to the end of dest
+ * Return: char(dest)
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int x = 0;
+	int i = 0;
+
+	while (dest[x] != '\0')
+	{
+		x++;
+ 	}
+	while ((src[i] != '\0') && (i < n))
+	{
+		dest[x] = src[i];
+		x++;
+		i++;
+	}
+	dest[x] = '\0';
+	return (dest);
+}
