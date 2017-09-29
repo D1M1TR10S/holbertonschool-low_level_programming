b136ba48993099778969895c11d62478dbc9ef2c
DimitiCat <dimitriosphilliou@gmail.com>
2017-09-29 06:16:32 +0000

    Made 6-cap!

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * *cap_string - Capitalize all the first letter of wrods in a string
+ * @c: pointer being returned to main.c file
+ * Return: c
+ */
+char *cap_string(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{	
+		if (c[i] == ' ' || c[i] == '.')
+		{
+			i++;
+			if (c[i] >= 97 && c[i] <= 122)
+				c[i] = c[i] - 32;
+		}
+		if (c[i] == '\t' || c[i] == '\n')
+		{
+			i++;
+			if (c[i] >= 97 && c[i] <= 122)
+				c[i] = c[i] - 32; 
+
+		}
+	}
+	return (c);
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -15,7 +15,7 @@ void reverse_array(int *a, int n);
 
 char *string_toupper(char *a);
 
-char *cap_string(char *);
+char *cap_string(char *c);
 
 char *leet(char *);
 
