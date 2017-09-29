a6a8785c89d0db7982694518b4836f2267add770
DimitiCat <dimitriosphilliou@gmail.com>
2017-09-29 03:17:04 +0000

    Made 4-rev_array.c

---
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * reverse_array - Reverse an array of integers 
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+	int c;
+	int i = 0;
+	n--;
+
+	while (n > i)
+	{
+		c = a[i];
+		a[i] = a[n];
+		a[n] = c;
+		n--;
+		i++;
+	}
+}
