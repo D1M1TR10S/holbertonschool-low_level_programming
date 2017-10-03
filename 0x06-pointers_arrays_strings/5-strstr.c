510b197fb8de16eb29dcb3c40c270c19100ff475
DimitiCat <dimitriosphilliou@gmail.com>
2017-10-03 05:42:18 +0000

    Print needle in haystack. Return substring of characters matching string.

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * *_strstr - Finds a substring within a string
+ * @haystack: Larger string where function is finding the substring
+ * @needle: Substring being returned
+ * Return: needle
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int n = 0;
+
+	while (haystack[i] != '\0')
+	{
+		if (haystack[i] == needle[n])
+		{
+			return (needle);
+			n++;
+		}
+		i++;
+	}
+	return (NULL);
+}
