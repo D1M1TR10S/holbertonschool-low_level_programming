2b83e31a6b11ca358ca4eec5c3c998f53ba7bf4b
DimitiCat <dimitriosphilliou@gmail.com>
2017-10-03 02:32:31 +0000

    Made 2-strchr. Locates 'l' in the string 's' and prints string after.

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * *_strchr - Return first occurence of character c in the string s
+ * @s: Pointer to string saying hello
+ * @c: Variable for letter 'l'
+ * Return: NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
