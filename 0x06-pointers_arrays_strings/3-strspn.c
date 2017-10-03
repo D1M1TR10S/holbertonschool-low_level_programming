1aebf6bfa03f97576976687814a6917b68f6d7dc
DimitiCat <dimitriosphilliou@gmail.com>
2017-10-03 04:55:23 +0000

    Removed one variable and made strspn work.

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -10,21 +10,20 @@ unsigned int _strspn(char *s, char *accept)
 	int i = 0;
 	int x = 0;
 	int n = 0;
-	int f = 0;
 
 	while (s[n] != '\0')
 	{
 		while (accept[x] != '\0')
 		{
 			if (s[n] == accept[x])
+			{	
 				i++;
-			else
-				f++;
+				break;
+			}
 			x++;
 		}
-		if (f == 4)
+		if (s[n] != accept[x])
 			break;
-		f = 0;
 		x = 0;
 		n++;
 	}
