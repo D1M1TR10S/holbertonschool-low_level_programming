8f489e813f6d50bfb912aa2825410f9e7c901dac
DimitiCat <dimitriosphilliou@gmail.com>
2017-10-03 00:09:33 +0000

    Made a memory set that prints 'b' 'n' number of times.

---
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * *_memset - prints constant byte 'b' 'n' number of times
+ * @b: Constant byte being printed
+ * @s: Pointer to destination
+ * @n: Unsigned int for number of times being copied
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*s = b;
+		s++;
+		i++;
+	}
+	return (s);
+}
