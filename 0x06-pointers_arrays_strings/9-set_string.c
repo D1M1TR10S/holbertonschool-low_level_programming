d33542093cea4d0eed0faa2a42de9448fae2bccf
DimitiCat <dimitriosphilliou@gmail.com>
2017-10-04 00:22:19 +0000

    Assigned a point to another pointer

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * set_string - Set **s to char *to
+ * @s: Pointer being sent to *to
+ * @to: Pointer holding value
+ * Return: Always 0.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
