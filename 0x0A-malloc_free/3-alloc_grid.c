9dedfea38fcd9c8a4caea1bacd50cbf795dfdfad
DimitiCat <dimitriosphilliou@gmail.com>
2017-10-11 04:55:10 +0000

    Print grid of 0's

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - Points to a 2-dimensional array of integers
+ * @width: width of the grid
+ * @height: height of the grid
+ * Return: ar
+ */
+int **alloc_grid(int width, int height)
+{
+	int i;
+	int j;
+	int **arr;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	arr = malloc(width * sizeof(int *));
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < width; i++)
+	{
+		arr[i] = malloc(height * sizeof(int *));
+		if (arr[i] == NULL)
+			return (NULL);
+	}
+	for (i = 0, j = 0; i < height; i++)
+	{
+		for ( ; j < width; j++)
+			arr[i][j] = 0;
+	}
+	return (arr);
+}
