e6b82f40b573c8ff4ef83f08defaeedd5acff142
DimitiCat <dimitriosphilliou@gmail.com>
2017-10-11 02:31:37 +0000

    Concatenated s1 and s1

---
+++ 0x0A-malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strlen - Check the length of a string
+ * @s: String being tested
+ * Return: l.
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+	return (l);
+}
+/**
+ * *str_concat - Concatenates two strings
+ * @s1: First string
+ * @s2: Second string
+ * Return: str.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *str;
+	int i;
+	int n = 0;
+
+	if (s1 == NULL)
+		s1 = '\0';
+	if (s2 == NULL)
+		s2 = '\0';
+
+	str = malloc((_strlen(s1) + _strlen(s2) + 1) * sizeof(char));
+
+	for (i = 0; s1[i] != '\0'; i++)
+		str[i] = s1[i];
+	while (s2[n] != '\0')
+	{
+		str[i] = s2[n];
+		i++;
+		n++;
+	}
+	return (str);
+}
+
