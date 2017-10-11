1e3a2819ee5e4e47c1fcd42d714eafbab63d9169
DimitiCat <dimitriosphilliou@gmail.com>
2017-10-11 00:20:45 +0000

    Removed whitespace

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -14,13 +14,11 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	
 	ar = malloc(size * sizeof(char));
-	
 	if (ar != NULL)
 	{
 		for (i = 0; i < size; i++)
 			ar[i] = c;
 	}
-	return(ar);
+	return (ar);
 }
