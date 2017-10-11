c975f101b32034dbc82f78bb94982abf18e86ede
DimitiCat <dimitriosphilliou@gmail.com>
2017-10-11 03:05:09 +0000

    Changed '\0' to 0

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -27,13 +27,18 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 	int i;
 	int n = 0;
+	int len1, len2;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		len1 = 0;
+	else
+		len1 = _strlen(s1);
 	if (s2 == NULL)
-		s2 = '\0';
+		len2 = 0;
+	else
+		len2 = _strlen(s2);
 
-	str = malloc((_strlen(s1) + _strlen(s2) + 1) * sizeof(char));
+	str = malloc((len1 + len2 + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
 	if (str != NULL)
