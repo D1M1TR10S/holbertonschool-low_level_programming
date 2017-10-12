59b22af88a411bbba2f00c4caa06d127e478c4d2
DimitiCat <dimitriosphilliou@gmail.com>
2017-10-12 23:26:32 +0000

    Return NULL if either value being passed is 0

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 /**
  * _calloc - Allocates memory for an array
- * @nmemb
- * @size
+ * @nmemb: Number of values in array
+ * @size: size of data type being passed
  * Return: arr
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
@@ -16,8 +16,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (arr == NULL)
 		return (NULL);
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
 	for (i = 0; i < (nmemb * size); i++)
 		arr[i] = 0;
-	
-	return((void *)arr);
+
+	return ((void *)arr);
 }
