ec995a3bd34d3b1674428ededa089a0a21afe944
DimitiCat <dimitriosphilliou@gmail.com>
2017-10-13 02:05:59 +0000

    Moved edge cases

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -12,11 +12,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	char *arr;
 
-	arr = malloc(nmemb * size);
-	if (arr == NULL)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	if (nmemb == 0 || size == 0)
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
 		return (NULL);
 
 	for (i = 0; i < (nmemb * size); i++)
