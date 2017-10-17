96d5e940f417fe6a5f8bc7e942a978dd6b288eaf
DimitiCat <dimitriosphilliou@gmail.com>
2017-10-17 03:16:36 +0000

    Name, age, and owner's name for a cute little pup

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - Structure of information about a dog.
+ * @name: Dog's name
+ * @age: Age of the dog
+ * @owner: Name of owner
+ * Description: name, age, and owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
