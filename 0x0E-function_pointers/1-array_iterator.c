4ba5ff782b851a2ab5074e65f7c8026d3e1d9cda
DimitiCat <dimitriosphilliou@gmail.com>
2017-10-18 22:54:46 +0000

    Iterate through an array and execute the function at that position

---
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - Executes each consecutive function within an array.
+ * @array: Pointer to the array
+ * @size: Size of the array
+ * @action: a pointer to the function being used
+ * Return: Void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	while (i < size)
+	{
+		(action)(array[i]);
+		i++;
+	}
+}
