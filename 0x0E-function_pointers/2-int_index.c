4dbaf381468179be9fb1a643ea7209e31776f008
DimitiCat <dimitriosphilliou@gmail.com>
2017-10-19 00:23:22 +0000

    Changed back into 3 returns

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -11,14 +11,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (array != NULL || cmp != NULL || size > 0)
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+
+	while (i < size)
 	{
-		while (i < size)
-		{
-			if (cmp(array[i]) != 0)
-				return (i);
-			i++;
-		}
+		if (cmp(array[i]) != 0)
+			return (i);
+		i++;
 	}
 	return (-1);
 }
