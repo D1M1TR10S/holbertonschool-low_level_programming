d363c7e52c17e676242f701f44cec3d9d15ae73f
DimitiCat <dimitriosphilliou@gmail.com>
2017-10-19 00:21:27 +0000

    Consolidated and removed lines

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -11,17 +11,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (array == NULL || cmp == NULL)
-		return (-1);
-
-	if (size <= 0)
-		return (-1);
-
-	while (i < size)
+	if (array != NULL || cmp != NULL || size > 0)
 	{
-		if (cmp(array[i]) != 0)
-			return (i);
-		i++;
+		while (i < size)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+			i++;
+		}
 	}
 	return (-1);
 }
