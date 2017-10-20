f8ef4bdab8efa5ef7af31705eac0d07702cf0eef
DimitiCat <dimitriosphilliou@gmail.com>
2017-10-20 21:49:19 +0000

    Print strings in a line

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Print numbers sent in as arguments
+ * @separator: Character printed between each argument in output
+ * @n: Number of integers to be printed
+ * @...: Variable arguments coming from standard input
+ * Return: Always 0.
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strings;
+	unsigned int i;
+
+	if (separator == NULL)
+		return;
+
+	va_start(strings, n);
+
+	for (i = 0; i < n - 1; i++)
+		printf("%s%s", va_arg(strings, char *), separator);
+	printf("%s\n", va_arg(strings, char *));
+
+	va_end(strings);
+}
