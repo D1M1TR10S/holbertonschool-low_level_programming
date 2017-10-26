0bec0a787b308fae60b0f87a18b0aa1d51257cda
DimitiCat <dimitriosphilliou@gmail.com>
2017-10-26 23:00:35 +0000

    Print length of string, followed by the string. Return the number of nodes

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - Print list of nodes and the length of each string
+ * 
+ * Return: Always 0.
+ */
+
+size_t print_list(const list_t *h)
+{
+	int n = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
---
+++ 0x11-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Making linked lists to organize a changing list of information using pointers and no arrays.
