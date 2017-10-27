18ed631e8d3f45c712c0470822a993c545a70c42
DimitiCat <dimitriosphilliou@gmail.com>
2017-10-27 21:42:57 +0000

    Checked pointers for NULL

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -16,19 +16,17 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	if (head == NULL)
 		return (NULL);
+	if (str == NULL)
+		return (NULL);
 
 	new_node = (list_t *) malloc(sizeof(list_t));
-
+	
+	if (new_node == NULL)
+		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 		;
-
 	new_node->len = i;
-	
-	if (strdup(str) != NULL)
-		new_node->str = strdup(str);
-	else
-		return (NULL);
-	
+	new_node->str = strdup(str);
 	new_node->next = NULL;
 
 	if (*head == NULL)
