37de089e208c5b39ab338f4287d8039b195263cc
DimitiCat <dimitriosphilliou@gmail.com>
2017-10-31 20:30:38 +0000

    Spaced listint_t pointer

--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,7 +13,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (head == NULL)
 		return (NULL);
 
-	new_node = (listint_t*) malloc(sizeof(listint_t));
+	new_node = (listint_t *) malloc(sizeof(listint_t));
 
 	if (new_node == NULL)
 		return (NULL);
