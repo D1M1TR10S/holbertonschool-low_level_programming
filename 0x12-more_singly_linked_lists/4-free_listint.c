50b43b3bc6bb7cd6d65735fe3ac3dfbe2630336e
DimitiCat <dimitriosphilliou@gmail.com>
2017-10-31 21:05:00 +0000

    Freed malloced space for #3

---
+++ 0x12-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - Frees linked list previously made
+ * @head: Pointer to first node
+ * Return: Void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}	
+}
