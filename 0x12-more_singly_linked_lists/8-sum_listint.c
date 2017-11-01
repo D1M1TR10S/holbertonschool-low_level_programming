530be1016e000696ca56c5bb4a3370d0306aa2bd
DimitiCat <dimitriosphilliou@gmail.com>
2017-11-01 04:02:09 +0000

    Removed white space

---
+++ 0x12-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * sum_listint - Return the sum of all values held
+ * @head: Pointer to first node
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
+}
