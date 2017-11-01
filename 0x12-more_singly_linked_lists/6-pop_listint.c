1288cfc92641c0ded1060f6498775f32a2d6b490
DimitiCat <dimitriosphilliou@gmail.com>
2017-11-01 00:04:42 +0000

    Pop the first node and return it's value

---
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * pop_listint - Return value of head, delete it, make next node new head
+ * @head: First node
+ * Return: num
+ */
+int pop_listint(listint_t **head)
+{
+	int num;
+	listint_t *temp;
+
+	if (*head == NULL)
+		return (0);
+
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+		return (0);
+
+	num = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	(*head) = temp;
+
+	return (num);
+}
