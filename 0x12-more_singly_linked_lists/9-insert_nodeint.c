3923a4353f0891412aeed65dd584eb5f2c4f1805
DimitiCat <dimitriosphilliou@gmail.com>
2017-11-01 05:55:34 +0000

    Removed unnecessary variables

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,15 +8,8 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *left;
-	unsigned int index;
 	listint_t *new_node;
-	listint_t *new_head = *head;
-
-	if (head == NULL)
-		return (NULL);
-	if (*head == NULL)
-		return (NULL);
+	listint_t *node = *head;
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
@@ -24,14 +17,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node->n = n;
 
-	for (index = 0; new_head != NULL && index != idx; index++)
+	if (idx == 0)
 	{
-		if (index + 1 == idx)
-			left = new_head;
-		new_head = new_head->next;
+		new_node->next = (*head);
+		(*head) = new_node;
+		return (*head);
 	}
-	new_node->next = new_head;
-	new_head = left;
-	left->next = new_node;
+	while (idx - 1)
+	{
+		--idx;
+		if (node->next == NULL)
+			return (NULL);
+		node = node->next;
+	}
+	new_node->next = node->next;
+	node->next = new_node;
 	return (new_node);
 }
