92005945c98f577a1882f1df2431ab5900626403
DimitiCat <dimitriosphilliou@gmail.com>
2017-11-08 02:16:59 +0000

    Read and print text file to standard output.

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+#include "holberton.h"
+/**
+ * read_textfile - Read a text file and print it to standard output
+ * @filename: String being printed in output
+ * @letters: Number of letters to read and print
+ * Return: Always 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	read(fd, buf, letters);
+	write(STDOUT_FILENO, buf, letters);
+	close(fd);
+
+	return (letters);
+}
---
+++ 0x14-file_io/README.md
@@ -0,0 +1,1 @@
+Working with File I/O, file descriptors, oflags, etc. Learning read(), write() and open() functions.
