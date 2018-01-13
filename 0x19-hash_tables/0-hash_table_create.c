f0df504aee8cae4771c886f880fcd6172c94504c
DimitiCat <dimitriosphilliou@gmail.com>
2018-01-13 00:42:38 +0000

    Added semicolons to assignments

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -12,11 +12,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size < 1)
 		return (NULL);
 
-	new_table = malloc(sizeof(hash_table_t))
+	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
 
-	new_table->array = malloc(sizeof(hash_node_t *) * size)
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table == NULL)
 		return (NULL);
 
