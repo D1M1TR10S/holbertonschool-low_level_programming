3ba6b00aa1224df77a83dc76da9439ebb8a996dc
DimitriCat <dimitriosphilliou@gmail.com>
2018-01-30 01:17:44 +0000

    Made selection sort function that sorts an array

---
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+/**
+ * selection_sort - Sorts an arrays with selection sort algorithm
+ * @array: Pointer to array being sorted
+ * @size: Size of array being sorted
+ * return: Nothing
+ */ 
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, s, idx;
+	int small;
+	int swap;
+
+	if (array == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		small = swap = array[i];
+		for (s = i; s < size; s++)
+		{
+			if (array[s] < small)
+			{
+				small = array[s];
+				idx = s;
+			}
+		}
+		if (small != swap)
+		{
+			array[i] = small;
+			array[idx] = swap;
+			print_array(array, size);
+		}
+	}	
+}
