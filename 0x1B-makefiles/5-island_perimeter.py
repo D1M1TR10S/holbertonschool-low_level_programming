6aa02ebda870243590fd02c576ac62f54259484f
DimitriCat <dimitriosphilliou@gmail.com>
2018-02-17 02:34:30 +0000

    Check for edge cases of island on the edge of matrix

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -12,12 +12,12 @@ def island_perimeter(grid):
     for y in range(len(grid)):
         for x in range(len(grid[y])):
             if grid[y][x] == 1:
-                if y > 0 and grid[y - 1][x] == 0:
+                if y - 1 < 0 or grid[y - 1][x] == 0:
                     perim += 1
-                if y < len(grid) and grid[y + 1][x] == 0:
+                if y + 1 > len(grid) or grid[y + 1][x] == 0:
                     perim += 1
-                if x > 0 and grid[y][x - 1] == 0:
+                if x - 1 < 0 or grid[y][x - 1] == 0:
                     perim += 1
-                if x < len(grid[y]) and grid[y][x + 1] == 0:
+                if x + 1 > len(grid[y]) or grid[y][x + 1] == 0:
                     perim += 1
     return perim
