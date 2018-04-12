6505b3912c9e2632a8f3c86211fe0773000e6755
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-04-12 00:20:18 +0000

    Added checker for an empty list

--- 0x1D-search_algorithms/100-jump.c
+++ 0x1D-search_algorithms/100-jump.c
@@ -15,32 +15,32 @@ int jump_search(int *array, size_t size, int value)
 {
 	unsigned int i, l, jump = sqrt(size);
 
-	if (array == NULL)
-		return (-1);
-
-	for (i = 0; i < size; i += jump)
+	if (array != NULL && size > 1)
 	{
-		if (array[i] >= value || (i + jump) > size)
+		for (i = 0; i < size; i += jump)
 		{
-			if ((i + jump) > size)
+			if (array[i] >= value || (i + jump) > size)
 			{
+				if ((i + jump) > size)
+				{
+					printf("Value checked array[%d] = [%d]\n", i, array[i]);
+					l = i;
+					i += jump;
+				}
+				else
+					l = i - jump;
+				printf("Value found between indexes [%d] and [%d]\n", l, i);
+				while (l <= i && l < size)
+				{
+					printf("Value checked array[%d] = [%d]\n", l, array[l]);
+					if (array[l] == value)
+						return (l);
+					l++;
+				}
+			}
+			if (i < size)
 				printf("Value checked array[%d] = [%d]\n", i, array[i]);
-				l = i;
-				i += jump;
-			}
-			else
-				l = i - jump;
-			printf("Value found between indexes [%d] and [%d]\n", l, i);
-			while (l <= i && l < size)
-			{
-				printf("Value checked array[%d] = [%d]\n", l, array[l]);
-				if (array[l] == value)
-					return (l);
-				l++;
-			}
 		}
-		if (i < size)
-			printf("Value checked array[%d] = [%d]\n", i, array[i]);
 	}
 	return (-1);
 }
