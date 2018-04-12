b8f015c100f5dbca87b1020a3db7cec7a2ed58d1
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-04-12 00:52:02 +0000

    Added whitespace

--- 0x1D-search_algorithms/100-jump.c
+++ 0x1D-search_algorithms/100-jump.c
@@ -37,7 +37,7 @@ int jump_search(int *array, size_t size, int value)
 			if (i < size)
 				printf("Value checked array[%d] = [%d]\n", i, array[i]);
 			if (array[i] == value)
-				return(i);
+				return (i);
 			i++;
 		}
 	}
